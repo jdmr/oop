9ef5a2af926a37e298959945e6fcc4b629048475
David Mendoza <jdmendozar@gmail.com>
2024-04-15 16:15:53 -0500

    Refactor safearay class and add exception handling for out of bounds access

--- ch14/live14/14exercise9.cpp
+++ ch14/live14/14exercise9.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-const int LIMIT = 100;
+const int LIMIT = 5;
 
 class safearay
 {
@@ -10,12 +10,22 @@ private:
 
 public:
     class OutOfBound
-    {
+    { 
+        public:
+        string message = "Index out of bounds";
+        
+        OutOfBound(string msg): message(msg) {     
+        }
+
     };
     int &operator[](int n)
     {
         if (n < 0 || n >= LIMIT)
-            throw OutOfBound();
+            throw OutOfBound(
+                "Index out of bounds: " 
+                + to_string(n) 
+                + " is greater than " 
+                + to_string(LIMIT - 1));
         return arr[n];
     }
 };
@@ -29,7 +39,7 @@ int main()
             sa1[i] = i * 10;
         }
 
-        for (int j = 0; j < LIMIT; j++)
+        for (int j = 0; j < LIMIT+1; j++)
         {
             int temp = sa1[j];
             cout << "Element " << j << " is " << temp << endl;
@@ -37,9 +47,9 @@ int main()
 
         return 0;
     }
-    catch (safearay::OutOfBound)
+    catch (safearay::OutOfBound ob)
     {
-        cout << "Index out of bounds" << endl;
+        cout << ob.message << endl;
         return 1;
     }
 }
